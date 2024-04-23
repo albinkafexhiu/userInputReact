b774f2a9438014edaa5b0f6a8f311093077b848f
albinkafexhiu <kafexhiu.22@gmail.com>
2024-04-24 01:03:01 +0200

    Updated project structure and components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import UsersList from "./Components/Users/UsersList";
+import AddUser from "./Components/Users/AddUser";
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
